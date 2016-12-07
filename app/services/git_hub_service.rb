2b1741f2da8ef2588a1949a9c94898ee2b2a2d03
Ben Pepper <bfpepper87@gmail.com>
2016-12-07 15:58:46 -0700

    add commits to index page.

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -3,27 +3,44 @@ class GitHubService
 
   def initialize(user)
     @user = user
-    @connection = Faraday.new("https://api.github.com/users/") do |faraday|
+    @connection = Faraday.new("https://api.github.com/") do |faraday|
       faraday.adapter Faraday.default_adapter
       faraday.headers["Authorization"] = "token #{user.token}"
     end
   end
 
   def get_repos
-    response = @connection.get "#{user.username}/repos"
+    response = @connection.get "users/#{user.username}/repos"
     parse(response.body).map do |repo|
       Repo.new(repo)
     end
   end
 
   def get_orgs
-    response = @connection.get('user/orgs')
+    response = @connection.get("users/#{user.username}/orgs")
     parse(response.body).map do |org|
       Org.new(org)
     end
   end
 
+  def get_commits
+    get_repo_commits(get_repos).flatten
+  end
+
   private
+
+  def get_repo_commits(all_repos)
+    all_repos.map do |repo|
+      repo_response = @connection.get("/repos/#{user.username}/#{repo.name}/commits?author=#{user.username}").body
+      set_commit_message(repo_response)
+    end
+  end
+
+  def set_commit_message(repo_response)
+    parse(repo_response).map do |commit|
+      Commit.new(commit)
+    end
+  end
 
   def parse(response)
     JSON.parse(response, symbolize_names: true)
